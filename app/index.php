7953828c15aea3fd51553ba432e8f90b3f51c777
astorcam <62352203+astorcam@users.noreply.github.com>
2021-10-15 10:14:38 +0200

    index toh guapo mejoh

--- app/index.php
+++ app/index.php
@@ -3,44 +3,30 @@
 
   echo '<link rel="stylesheet" type="text/css" href="style.css">';
   echo '
-  <div id="cajita">
-      <center>
-      <a href="registro.html">Registro</a><br><br>
-      <a href="login.html">Login</a>
-      </center>
-  
+  <head>
+    <link rel="icon" href="images/logo.png">
+    <link rel="stylesheet" type="text/css" href="style.css">
+      <title>
+        Lolipop
+      </title>
+  </head>
+  <body background="images/fondo.gif">
+  <form>
+	<div id="cajita">
+		<center><t> Loli<b><t class="c">pop</t></b></t><br><br>
+            	<img src="images/logo.png" alt="Lolipop Logo" style="width:70px;height:70px";><br><br>
+          	</center>
+	      <center>
+	      <a href="registro.html"><b><n>Registro</a><br><br>
+	      <a href="login.html"><b><n>Login</a>
+	      </center>
+      </div>
+  </form>
+</body>
   ';
 
 
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
-
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-  }
-
-
-
-
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-    <td>asdasd</td><br>
-   </tr>
-   ";
-
-
-}
+  
 echo'</div>'
 
 ?>
