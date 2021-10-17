0b29ea3a145d3432294343746b65109f2ef740df
iozalla <iozono11@gmail.com>
2021-10-17 18:13:07 +0200

    Editar elementos

--- app/verElementos.php
+++ app/verElementos.php
@@ -21,19 +21,30 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {//si el usua
   $stmt->execute();
   $result = $stmt->get_result(); // conseguir el resultado sql
   while ($row = mysqli_fetch_array($result)) {
+    $edad=$row['edad'];
+    $gustos=$row['gustos'];
+    $id=$row['id'];
+    $altura=$row['altura'];
+    $peso=$row['peso'];
+    $sexo=$row['sexo'];
     //importante cuando haga lo de editar hay que mirar si quien va a editar el elemento es la persona que lo ha creado, solo en ese caso sera capaz de editarlo, si no se podria editar algo que no te pertenece ya que solo se miraria el id del elemento que se quiere editar
     echo
     "
-    <form method='POST'  action='editar.php?fid=&follow=False'><tr>
-      <td>gustos: {$row['gustos']}</td>
-      <td>edad: {$row['edad']}</td>
-      <td>peso: {$row['peso']}</td>
-      <td>altura: {$row['altura']}</td>
-      <td>sexo: {$row['sexo']}</td>
+    <form method='POST'  action='editarElemento.php?fid=$id'><tr>
+      <td>gustos: </td>
+      <n><b>GUSTOS(separados por coma):<br></n></b><textarea id='fgustos' name='fgustos' rows='4' cols='50'>$gustos</textarea><br><br>
+      <b><n>EDAD:        <input type='text' id='fedad' name='fedad' value='$edad'>
+      <th><b><n>ALTURA:</th>    <th><input type='text' id='faltura' name='faltura' value=$altura>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</th>
+      <th><b><n>PESO:</th>   <th><input type='text' id='fpeso' name='fpeso' value=$peso></th>
+      <th><b><n>PESO:</th>   <th><input type='text' id='fsexo' name='fsexo' value=$sexo></th>
+
+
 
 
       <input type='submit' value='editar'>
       </form>
+
+      <br>______________________________________________________________
       </tr><br><br>";
     }
 
