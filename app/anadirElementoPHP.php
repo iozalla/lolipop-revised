2002032b87586be6158804da7eafc92b3d9283aa
astorcam <astorcam@gmail.com>
2021-12-19 13:28:32 +0100

    sanitizados vlaues de elementos
    si

--- app/anadirElementoPHP.php
+++ app/anadirElementoPHP.php
@@ -31,10 +31,12 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 
  $fgustos=htmlspecialchars($_GET["fgustos"]); //Sanitizamos el input del usuario para evitar ataques XSS alamcenados
  $fsexo=htmlspecialchars($_GET["fsexo"]); //Sanitizamos el input del usuario para evitar ataques XSS alamcenados
+$faltura=htmlspecialchars($_GET["faltura"];
+ $fpeso=htmlspecialchars($_GET["fpeso"];
  $sql = "INSERT INTO elementos (mail,gustos, edad, altura, peso, sexo) VALUES (?,?,?,?,?,?); ";
 
  $stmt= $conn->prepare($sql);//prepara el texto sql para que no haya i  nyecciones sql
- $stmt->bind_param("ssssss",$_SESSION['username'], $fgustos ,$_GET["fedad"], $_GET["faltura"], $_GET["fpeso"], $fsexo);//asigna los parametros
+ $stmt->bind_param("ssssss",$_SESSION['username'], $fgustos ,$_GET["fedad"], $faltura, $fpeso, $fsexo);//asigna los parametros
  $gustos=htmlspecialchars($_GET["fgustos"]);
 
 if ($stmt->execute()) {//ejecuta la instruccion sql
